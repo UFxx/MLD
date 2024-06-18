6468dd84e3c54aa929eb5089661888a63d8f8320
UFxx <smileprost@gmail.com>
2024-06-18 20:28:59 +0300

    Delete node

--- assets/js/notes/addNote.js
+++ assets/js/notes/addNote.js
@@ -3,12 +3,12 @@ const notesContainer = document.querySelector(".notes");
 
 addNoteBtn.addEventListener("click", () => {
   const note = document.createElement("div");
-  note.classList.add("note")
-  
+  note.classList.add("note");
+
   const noteTitle = document.createElement("input");
   noteTitle.setAttribute("placeholder", "Заметка");
   noteTitle.setAttribute("type", "text");
-  
+
   const noteText = document.createElement("textarea");
   noteText.setAttribute("placeholder", "Начните че-то писать");
 
