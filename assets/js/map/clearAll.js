750ba3501b7b618dbd569dad31807468f42b8ca6
UFxx <smileprost@gmail.com>
2024-09-15 19:04:49 +0300

    Fix map bug

--- assets/js/map/clearAll.js
+++ assets/js/map/clearAll.js
@@ -4,10 +4,12 @@ import { setStartPositions } from "./playerMovement.js";
 
 const clearAll = document.querySelector(".clear-all");
 const mapSettings = document.querySelector(".map-settings");
+const otherModes = document.querySelector('.other-modes')
 
 clearAll.addEventListener("click", () => {
   clearHeroes();
   clearArrows();
   setStartPositions();
   mapSettings.classList.remove("map-settings__visible");
+  otherModes.style.display = 'flex'
 });
