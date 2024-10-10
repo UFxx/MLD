fb23594c1ecd08a29263f1b7bfa57bfe320e594e
UFxx <smileprost@gmail.com>
2024-10-10 20:26:39 +0300

    Fixed the heroe's choise on map settings

--- assets/js/map/changeHero.js
+++ assets/js/map/changeHero.js
@@ -1,30 +1,34 @@
-import { clearAcitveHeroes } from "./mapSettings.js";
+import { clearAcitveHeroes } from './mapSettings.js';
 
-const settingsPlayers = document.querySelectorAll(".map-heroes-item-img");
-const blueTeamPlayers = document.querySelectorAll(".player-blue-team");
-const redTeamPlayers = document.querySelectorAll(".player-red-team");
+const settingsPlayers = document.querySelectorAll('.map-heroes-item-img');
+const blueTeamPlayers = document.querySelectorAll(
+  '.player-blue-team img:first-child'
+);
+const redTeamPlayers = document.querySelectorAll(
+  '.player-red-team img:first-child'
+);
 
 settingsPlayers.forEach((settingPlayer, i) => {
-  settingPlayer.addEventListener("click", () => {
-    if (settingPlayer.getAttribute("src") === "./assets/images/empty.png") {
-      const activeHero = document.querySelector(".map__hero-active");
-      settingPlayer.setAttribute("src", activeHero.getAttribute("src"));
-      settingPlayer.setAttribute("alt", activeHero.getAttribute("alt"));
+  settingPlayer.addEventListener('click', () => {
+    if (settingPlayer.getAttribute('src') === './assets/images/empty.png') {
+      const activeHero = document.querySelector('.map__hero-active');
+      settingPlayer.setAttribute('src', activeHero.getAttribute('src'));
+      settingPlayer.setAttribute('alt', activeHero.getAttribute('alt'));
       clearAcitveHeroes();
     } else {
-      settingPlayer.setAttribute("src", "./assets/images/empty.png");
-      settingPlayer.setAttribute("alt", "");
+      settingPlayer.setAttribute('src', './assets/images/empty.png');
+      settingPlayer.setAttribute('alt', '');
     }
 
     try {
       redTeamPlayers[(i - 1) / 2].setAttribute(
-        "src",
-        settingPlayer.getAttribute("src")
+        'src',
+        settingPlayer.getAttribute('src')
       );
     } catch {}
     blueTeamPlayers[i / 2].setAttribute(
-      "src",
-      settingPlayer.getAttribute("src")
+      'src',
+      settingPlayer.getAttribute('src')
     );
   });
 });
