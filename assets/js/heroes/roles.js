8ffa2fb2baeabcf558b3ff9bbc86ee7f9c0f726f
UFxx <smileprost@gmail.com>
2024-10-10 20:16:11 +0300

    Scroll to top when sort by role

--- assets/js/heroes/roles.js
+++ assets/js/heroes/roles.js
@@ -1,21 +1,23 @@
-const roles = document.querySelectorAll(".roles ul li button");
-const heroes = document.querySelectorAll(".hero");
+const roles = document.querySelectorAll('.roles ul li button');
+const heroes = document.querySelectorAll('.hero');
+const heroesContainer = document.querySelector('.heroes-container');
 
 roles.forEach((role) =>
-  role.addEventListener("click", () => {
+  role.addEventListener('click', () => {
+    heroesContainer.scrollTo(0, 0);
     roles.forEach((role) => {
-      role.classList.remove("role__active");
+      role.classList.remove('role__active');
     });
-    role.classList.add("role__active");
+    role.classList.add('role__active');
 
     heroes.forEach((hero) => {
-      if (hero.getAttribute("role") !== role.value) {
-        hero.style.display = "none";
+      if (hero.getAttribute('role') !== role.value) {
+        hero.style.display = 'none';
       } else {
-        hero.style.display = "block";
+        hero.style.display = 'block';
       }
-      if (role.value === "All") {
-        hero.style.display = "block";
+      if (role.value === 'All') {
+        hero.style.display = 'block';
       }
     });
   })
