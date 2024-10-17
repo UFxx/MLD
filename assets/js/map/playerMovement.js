8b661ad4432cb44433ae975129d09611649c3bbf
UFxx <smileprost@gmail.com>
2024-10-17 11:22:06 +0300

    Player positions for different resolutions

--- assets/js/map/playerMovement.js
+++ assets/js/map/playerMovement.js
@@ -1,22 +1,45 @@
 const canvas = document.querySelector('#map-canvas');
 const players = document.querySelectorAll('.player-container');
 
-const playersStartPositions = [
-  [
-    { startPositionX: 220, startPositionY: 110 },
-    { startPositionX: 1200, startPositionY: 650 },
-    { startPositionX: 680, startPositionY: 360 },
-    { startPositionX: 470, startPositionY: 400 },
-    { startPositionX: 600, startPositionY: 330 }
-  ],
-  [
-    { startPositionX: 350, startPositionY: 50 },
-    { startPositionX: 1350, startPositionY: 550 },
-    { startPositionX: 800, startPositionY: 300 },
-    { startPositionX: 1000, startPositionY: 240 },
-    { startPositionX: 750, startPositionY: 240 }
-  ]
-];
+let playersStartPositions;
+
+console.log(window.innerWidth);
+
+if (window.innerWidth === 1536) {
+  playersStartPositions = [
+    [
+      { startPositionX: 220, startPositionY: 110 },
+      { startPositionX: 1200, startPositionY: 650 },
+      { startPositionX: 680, startPositionY: 360 },
+      { startPositionX: 470, startPositionY: 400 },
+      { startPositionX: 600, startPositionY: 330 }
+    ],
+    [
+      { startPositionX: 350, startPositionY: 50 },
+      { startPositionX: 1350, startPositionY: 550 },
+      { startPositionX: 800, startPositionY: 300 },
+      { startPositionX: 1000, startPositionY: 240 },
+      { startPositionX: 750, startPositionY: 240 }
+    ]
+  ];
+} else if (window.innerWidth === 1920) {
+  playersStartPositions = [
+    [
+      { startPositionX: 270, startPositionY: 150 },
+      { startPositionX: 1500, startPositionY: 850 },
+      { startPositionX: 850, startPositionY: 450 },
+      { startPositionX: 600, startPositionY: 550 },
+      { startPositionX: 720, startPositionY: 430 }
+    ],
+    [
+      { startPositionX: 500, startPositionY: 50 },
+      { startPositionX: 1700, startPositionY: 700 },
+      { startPositionX: 1000, startPositionY: 400 },
+      { startPositionX: 1250, startPositionY: 320 },
+      { startPositionX: 920, startPositionY: 320 }
+    ]
+  ];
+}
 
 export function setStartPositions() {
   const blueTeamPlayers = document.querySelectorAll('.player-blue-team');
